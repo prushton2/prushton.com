c160755b34e0183886403d40636f2ecad4f634e6
prushton2 <peterrushton418@gmail.com>
2022-07-06 22:28:15 -0400

    added config

---
+++ config.js
@@ -0,0 +1,1 @@
+export const thisURL = "http://localhost:8080"
--- cors.js
+++
@@ -1,9 +0,0 @@
-const getURLWithoutCors = async(url) => {
-    response = await fetch("https://corsanywherepr.herokuapp.com/"+url).then((data) => {
-        responsejson = data.text().then((value) => {
-            return value
-        });
-        return responsejson;
-    });
-    return response
-}
