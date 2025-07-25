3ac732d110865a7a74eb940bbfa5ad664d310584
prushton2 <peterrushton418@gmail.com>
2025-07-25 12:19:53 -0400

    Looks sane now

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -3,17 +3,16 @@ import type { Project } from "../models/Project"
 import "./Project.css"
 
 function ProjectComponent({project}: {project: Project}) {
-    const [clicked, setClicked] = useState<boolean>(false)
 
-    const expandedJSX: JSX.Element = <>
-        <a className="ProjectLink" href={project.repo.url}>{project.repo.plaintext}</a>
-        <a className="ProjectLink" href={project.url.url}>{project.url.plaintext}</a>
-    </>
-
-    return <div className="ProjectContainer" onClick={() => setClicked(!clicked)}>
+    return <div className="ProjectContainer">
         <label className="ProjectNameLabel">{project.name}</label>
-        <label className="ProjectDescLabel">{project.desc}</label>
-        {clicked ? expandedJSX: <></>}
+        <div className="ProjectInfoContainer">
+            <label className="ProjectDescLabel">{project.desc}</label>
+            <div className="LinkContainer">
+                <a className="ProjectLink" href={project.repo.url}>{project.repo.plaintext}</a>
+                <a className="ProjectLink" href={project.url.url}>{project.url.plaintext}</a>
+            </div>
+        </div>
     </div>
 }
 
