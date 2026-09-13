1bcdc86d756bd661c2b7a00354b75dd88f68a43f
prushton2 <peterrushton418@gmail.com>
2026-09-13 00:46:10 -0400

    Add qr page

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -6,5 +6,11 @@ export const Pages: Page[] = [
         desc: "Ever want to know how many bats are needed to lift an object? Me neither!",
         page: "https://bat.prushton.com",
         repo: "https://github.com/prushton2/bat",
+    },
+    {
+        name: "QR",
+        desc: "Generate free authentic QR Codes",
+        page: "https://qr.prushton.com",
+        repo: "https://github.com/prushton2/qr",
     }
 ]
